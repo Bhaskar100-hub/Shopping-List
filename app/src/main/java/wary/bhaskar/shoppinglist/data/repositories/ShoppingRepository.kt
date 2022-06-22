5c7409415b85ebe06d041182aa2e0286571807f2
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-22 10:19:16 +0530

    Added Shopping Repository under repositories package

---
+++ app/src/main/java/wary/bhaskar/shoppinglist/data/repositories/ShoppingRepository.kt
@@ -0,0 +1,14 @@
+package wary.bhaskar.shoppinglist.data.repositories
+
+import wary.bhaskar.shoppinglist.data.db.ShoppingDatabase
+import wary.bhaskar.shoppinglist.data.db.entities.ShoppingItem
+
+class ShoppingRepository(
+    private val db: ShoppingDatabase
+) {
+    suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
+}
